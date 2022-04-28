bd0036ce035554de45741ab052f5158dc14a2640
Aniket-we <aniketmudawar899@gmail.com>
2022-04-28 12:37:32 +0530

    new code

--- commit.sh
+++ commit.sh
@@ -38,6 +38,19 @@ echo "Selected file =====> " $file
 echo "\n"
 echo "${heading_color}******************* Adding files for commit  ***********************${NC}${text_color}" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
 echo "\n"
+echo "\n"
+
+count=0
+total=34
+pstr="[==================================]"
+
+while [ $count -lt $total ]; do
+  sleep 0.5 # this is work
+  count=$(( $count + 1 ))
+  pd=$(( $count * 73 / $total ))
+  printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
+done
+
 
 git add $file
 
