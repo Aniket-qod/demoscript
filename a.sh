2696f4db79837853b4319e1abe571cf6e01c5ce4
Aniket-we <aniketmudawar899@gmail.com>
2022-04-28 13:08:33 +0530

    new changes

--- a.sh
+++ a.sh
@@ -1,11 +1,11 @@
 count=0
-total=34
-pstr="[==================================]"
+total=30
+pstr="[===========================================]"
 
 while [ $count -lt $total ]; do
   sleep 0.5 # this is work
   count=$(( $count + 1 ))
-  pd=$(( $count * 73 / $total ))
+  pd=$(( $count * 45 / $total ))
   printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
 done
 
--- b.sh
+++ b.sh
@@ -50,7 +50,7 @@ progreSh() {
     done
 }
 
-printf "\n\n\n\n\n\n\n\n\n\n"
+printf "\n\n"
 progreSh 0
 progreSh 10
 progreSh 20
@@ -62,7 +62,7 @@ progreSh 70
 progreSh 80
 progreSh 90
 progreSh 100
-printf "\n\n\n\n\n\n\n\n\n\n"
+printf "\n\n"
 
 
 
@@ -79,22 +79,6 @@ printf "\n\n\n\n\n\n\n\n\n\n"
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo "\n"
-echo "\n
 
 
 
