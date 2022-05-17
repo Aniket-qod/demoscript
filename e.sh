d3bf0e3600371eb49213e7be28cead5841bff6d3
Aniket-qod <86476620+Aniket-qod@users.noreply.github.com>
2022-05-17 16:33:28 +0530

    added new code

--- e.sh
+++ e.sh
@@ -1,3 +1,11 @@
+for i in $(seq 1 100)
+do
+    sleep 0.1 
+    echo $i
+done | whiptail --title 'Test script' --gauge 'Running...' 6 60 0
+
+
+
 #whiptail --yes-button "Yeah" --no-button "Nope" --title "Choose the answer" --yesno "Will you choose yes?" 10 30
 
 
